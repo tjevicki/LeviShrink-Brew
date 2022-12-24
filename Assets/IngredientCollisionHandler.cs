49cb645dc527249cf00893404bb162d15fa2f936
Tibor Jevicki <tjevicki@outlook.com>
2022-12-24 13:31:19 +0100

    Add grab interaction and pickable objects

---
+++ Assets/IngredientCollisionHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientCollisionHandler : MonoBehaviour
+{
+    private List<string> addedIngredients = new List<string>();
+
+    // Possible ingredients:
+    // Gold
+    // Wax
+    // GoatStomachAcid
+    // HemorroidExtract
+    // DogVomit
+
+    void OnCollisionEnter(Collision collision)
+    {
+        PotionIngredient collidingIngredient = collision.gameObject.GetComponent<PotionIngredient>();
+
+        if (collidingIngredient != null)
+        {
+            Debug.Log("Added a " + collidingIngredient.ingredientId + " into the cauldron.");
+            addedIngredients.Add(collidingIngredient.ingredientId);
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
